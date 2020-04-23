93936cf0300756ebc2a0296477edb9919bb1b355
Виталий Кушла <KushlaVR@gmail.com>
2020-04-23 23:32:09 +0300

    Create project

---
+++ WebRemote/WebRemote.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		WebRemote.ino
+ Created:	4/23/2020 11:30:33 PM
+ Author:	Віталік
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
