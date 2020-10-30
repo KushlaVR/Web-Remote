d221bee6d5a66fba2d0b6c738472aedbeb240c21
Виталий Кушла <KushlaVR@gmail.com>
2020-10-30 17:28:18 +0200

    Покращення інтерфейсу

--- tools/copy.bat
+++ tools/copy.bat
@@ -1,3 +1,3 @@
-COPY "c:\users\%username%\appdata\local\Temp\VMBuilds\WemosRemote\esp8266_d1_mini\Release\WemosRemote.ino.bin" firmware.bin
-COPY "c:\users\%username%\appdata\local\Temp\VMBuilds\WemosRemote\esp8266_d1_mini\Release\8ef9dc2d6ccc632bd183ad5e6a315ab5\WemosRemote.ino.spiffs.bin" spiffs.bin
+COPY "c:\users\%username%\appdata\local\Temp\VMBuilds\WebRemote\esp8266_d1_mini\Release\WebRemote.ino.bin" firmware.bin
+COPY "c:\users\%username%\appdata\local\Temp\VMBuilds\webRemote\esp8266_d1_mini\Release\a44e068f96857bd535a6a130ca92924d\WebRemote.ino.spiffs.bin" spiffs.bin
 pause
